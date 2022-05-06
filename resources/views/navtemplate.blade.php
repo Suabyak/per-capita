fceb51ca0d09975eac6cf43e57020c6ec3b70d17
Jakub Wójcik <jakub.wojcik@stud.pwsz.legnica.edu.pl>
2022-05-06 21:54:29 +0200

    frontend fix

--- resources/views/navtemplate.blade.php
+++ resources/views/navtemplate.blade.php
@@ -109,8 +109,8 @@
 @yield('content')
 
 
-<script src="/../js/fade.js"></script>
-<script src="/../js/bootstrap.min.js"></script>
+<script src="js/fade.js"></script>
+<script src="js/bootstrap.min.js"></script>
 
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
 <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
---
+++ resources/views/template.blade.php
@@ -0,0 +1,72 @@
+{{--
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Per Capita @yield('title')</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="https://fonts.googleapis.com/css2?family=Hubballi&family=Lato:wght@100&family=Source+Sans+Pro:wght@200&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/x-icon" href="{{ url('img/pizza-favicon.png') }}">
+    <link rel="stylesheet" href="{{ url('/../css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ url('/../css/styles.css') }}">
+    <link rel="stylesheet" href="{{ url('/../css/register.css') }}">
+    <link rel="stylesheet" href="{{ url('/../css/register_first_time.css') }}">
+
+
+
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@200;400;600&display=swap" rel="stylesheet">
+</head>
+
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div class="container">
+            <a class="navbar-brand" href="/">
+                <img src="{{ '/../img/pizza-logo.png' }}" alt="Logo Per Capita">
+<!--                https://www.flaticon.com/free-icons/pizza-->
+                <div class="navbar-brand-overlay"></div>
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/order">menu</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">kontakt</a>
+                    </li>
+                    <li class="nav-item">
+                        <a onclick="show_hide()" class="nav-link" href="{{ route('login') }}">{{ __('zaloguj się') }}</a>
+                    </li>
+                </ul>
+                <ul class="elements">
+                    <li class="navitem">
+                        <a class="navlink " aria-current="page" href="/order">
+                            <span class="text">menu</span>
+                            <span class="icon"><ion-icon name="book-outline"></ion-icon></span></a>
+                    </li>
+                    <li class="navitem">
+                        <a class="navlink" href="#">
+                            <span class="text">kontakt</span>
+                            <span class="icon"><ion-icon name="call-outline"></ion-icon></span></a>
+                    </li>
+                    <li class="navitem-2">
+                        <a href="{{ route('login') }}">
+                            <button onclick="show_hide()" class="icon-2"><ion-icon name="log-in-outline"></ion-icon></button>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+@yield('content')
+--}}
+
+
